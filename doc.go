ffc0818242855216ae99c06ca802b71af1533155
Kevin Wiesmüller <kwiesmueller@seibert-media.net>
2018-04-17 01:03:05 +0200

    add doc.go

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright 2018 PlayNet <play-net.org>. All rights reserved.
+// Use of this source code is governed by a
+// license that can be found in the LICENSE file.
+
+/*
+Package gorcon is an application providing abstraction layers to map common rcon functionality from various games to a unified api (grpc/rest).
+Aside from this gorcon provides local management functionality to make running game servers easier (like starting, waching and scheduling).
+*/
+package gorcon
